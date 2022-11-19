27f70c4994644200e91f5b412db1995a16804c1e
Asam237 <abbasalimokolo@gmail.com>
2022-11-19 05:37:33 +0100

    feat: add theme and hack tailwind container

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1 class="text-3xl font-bold underline text-red-700">
-    Hello world!
-  </h1>
+  <div class="bg-primary">
+    <div class="container">
+      <h1 class="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -8,7 +9,38 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      // Override tailwind fontFamily
+      sans: ["Gibson", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#c1c1c1",
+        },
+        secondary: {
+          DEFAULT: "#ffffff",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      // Create our own container component
+      // and ask tailwind to take it into account.
+      addComponents({
+        ".container": {
+          maxWidth: "80vw",
+          marginLeft: "auto",
+          marginRight: "auto",
+          "@screen sm": {
+            maxWidth: "70vw",
+          },
+          "@screen xl": {
+            maxWidth: "980px",
+          },
+        },
+      });
+    },
+  ],
 };
